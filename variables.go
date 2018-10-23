7ca102632a37afdbda83a797bd10e639b16b60b2
totala <anand.totala@dell.com>
2018-10-22 22:25:17 -0700

    Variables in go

---
+++ variables.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+func main(){
+	var a string
+	a = "ANAND"
+	fmt.Println(a)
+
+	var b, c int = 1,2
+	fmt.Println("b + c",  b + c)
+
+	d := "Totala"
+	fmt.Println(a+ " "  + d)
+
+}
